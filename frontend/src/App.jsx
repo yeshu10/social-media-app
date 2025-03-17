10410ae7f1c1238587f92f435490771f98c3a5c9
yeshu10 <yeshvigupta10@gmail.com>
2025-03-17 19:55:44 +0530

    Initial commit: Set up project structure

---
+++ frontend/src/App.jsx
@@ -0,0 +1,25 @@
+
+import './App.css'
+
+function App() {
+  
+
+  return (
+    <>
+       <h1 class="text-3xl font-bold underline">
+    Hello world!
+  </h1>
+      <div className="card">
+        
+        <p>
+          Edit <code>src/App.jsx</code> and save to test HMR
+        </p>
+      </div>
+      <p className="read-the-docs">
+        Click on the Vite and React logos to learn more
+      </p>
+    </>
+  )
+}
+
+export default App
